e5468f17a60a92b208776e24a744cea71059e647
Serg Sosnin <sosnin.serg@gmail.com>
2020-10-04 20:48:55 +0300

    add main page, header

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 import "./import/modules";
+
+let icon_menu = document.querySelector('.icon-menu');
+icon_menu.addEventListener("click", function (e) {
+    this.classList.toggle('icon-menu_active');
+    let menu_body = document.querySelector('.menu__body');
+    menu_body.classList.toggle('menu__body_active')
+});
